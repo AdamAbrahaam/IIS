e2c04c1420cd80485581932156c1d8ed3f88c452
Adam Abrahám <adam.shadow77@hotmail.com>
2019-10-06 14:06:56 +0200

    init

---
+++ IIS/client-app/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="info">{{ info }}</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+
+  data() {
+    return {
+      info: null
+    };
+  },
+
+  mounted() {
+    axios.get("/weatherforecast").then(response => (this.info = response));
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
